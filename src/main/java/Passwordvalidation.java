94ac9699d2da90db4b42301609f880139eb1a8ab
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2023-11-07 11:16:00 +0100

    Codes zu Aufgaben und Tests zu checkForLength und checkForNumbers geschrieben

--- src/main/java/Passwordvalidation.java
+++ src/main/java/Passwordvalidation.java
@@ -1,2 +1,25 @@
 public class Passwordvalidation {
+
+    static String password;
+
+
+    public static boolean checkForLength(String password) {
+        if (password.length() >= 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean checkForNumbers(String password) {
+        char[] chars = password.toCharArray();
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
 }
+
--- src/test/java/PasswordvalidationTest.java
+++ src/test/java/PasswordvalidationTest.java
@@ -1,5 +1,55 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PasswordvalidationTest {
 
 
+    @Test
+    void PasswordvalidationTest_whenpasswordAtLeast8Characters_returnTrue() {
 
-}
+        // GIVEN
+
+        String password = "2Java0Boot2Camp3";
+
+        // WHEN
+
+        boolean checkForLength = Passwordvalidation.checkForLength(password);
+
+        // THEN
+        assertTrue(checkForLength);
+    }
+
+    @Test
+    void PasswordvalidationTest_ifPasswordIncludesNumbers_returnTrue() {
+        // GIVEN
+
+        String password = "2Java0Boot2Camp3";
+
+        // WHEN
+
+        boolean checkForNumbers = Passwordvalidation.checkForNumbers(password);
+
+        // THEN
+
+        assertTrue(checkForNumbers);
+
+    }
+
+    @Test
+    void PasswordvalidationTest_ifPasswordIncludesUpperandLowercase_returnTrue() {
+        //GIVEN
+
+        String password = "2Java0Boot2Camp3";
+
+        // WHEN
+
+        boolean checkForUpperAndLowercase = Passwordvalidation.checkForUpperAndLowercase(password);
+
+        // THEN
+         assertTrue(checkForUpperAndLowercase);
+
+    }
+
+    }
